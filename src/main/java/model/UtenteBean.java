4086b66205b00c5b049ef0ea8bbed1221337715e
carminemea <carminemea2005@gmail.com>
2026-05-25 23:43:40 +0200

    UtenteBean.java created

---
+++ src/main/java/model/UtenteBean.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.io.Serializable;
+
+public class UtenteBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	private String cognome;
+	private String email;
+	private String password;
+	private String ruolo;
+	
+	public UtenteBean() {
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getRuolo() {
+		return ruolo;
+	}
+	
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+
+}
